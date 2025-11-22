eecbbcba94963d9dcaac3e4065109c52e81334e7
Karan Kumar <karansprojects227@gmail.com>
2025-11-22 14:23:24 +0530

    Update page.js

--- app/auth/verify-otp/page.js
+++ app/auth/verify-otp/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import toast from "react-hot-toast";
-import { useSearchParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function VerifyOtpPage() {
   const router = useRouter();
@@ -138,5 +138,6 @@ export default function VerifyOtpPage() {
     </div>
   );
 }
+
 
 
